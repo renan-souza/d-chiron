9f72fc641a96499c615df1ed19287064b69bd6a9
Pedro Miranda <miranda.pp@gmail.com>
2016-05-06 20:07:39 -0300

    ADD rfa-spark run.sh instructions

--- rfa-synthetic/rfa-spark/run.sh
+++ rfa-synthetic/rfa-spark/run.sh
@@ -9,7 +9,7 @@ WFDIR=$REPO_HOME/rfa-synthetic/rfa-spark
 MASTER_HOST=$1
 SWB_PY=$REPO_HOME/rfa-synthetic/rfa-activities/bin/swb.py
 OUTPUT_DIR=${WFDIR}/out
-INPUT_FILE_PATH=input.dataset
+INPUT_FILE_PATH=${WFDIR}/input.dataset
 
 EXECUTORS=$2
 TOTAL_CORES=$3
@@ -17,9 +17,19 @@ TAG_NAME="SparkSyntheticRFA"
 BALANCED=true
 ENABLERANDOM=false
 
-
-
 ARGS="$MASTER_HOST $TOTAL_CORES $INPUT_FILE_PATH $TAG_NAME $OUTPUT_DIR $BALANCED ${ISDEBUG} $PATH_FILES $ACTIVATORJAR $SWB_PY ${ENABLERANDOM}"
 
-echo $ARGS
+# if number of args is wrong
+if [ $# != 3 ]; then
+  echo "Usage: run.sh <spark-master-url> <num-executors> <total-executor-cores>"
+  echo "Please check https://github.com/vssousa/d-chiron/tree/master/rfa-synthetic/rfa-spark"
+  exit 1
+fi
+
+# if number $SPARK_HOME is not set
+if [[ -z $SPARK_HOME ]]; then
+    echo "ERROR : SPARK_HOME is not a set"
+    exit 1
+fi
+
 $SPARK_HOME/bin/spark-submit --class $MAIN_CLASS --num-executors $EXECUTORS --total-executor-cores $TOTAL_CORES --master $MASTER_HOST $JAR_FILE $ARGS
